08dfd6f7df9b6d3ff9310dc2d807ac20882c59b9
hikaru-ida <iidhkr@gmail.com>
2017-03-08 02:49:19 +0900

    add code to add task to database

--- command/add.go
+++ command/add.go
@@ -1,14 +1,40 @@
 package command
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+	"strconv"
 	"strings"
+	"time"
 )
 
 type AddCommand struct {
-	Meta
 }
 
 func (c *AddCommand) Run(args []string) int {
+	for i := 0; i < len(args); i++ {
+		fmt.Println(args[i])
+	}
+	if len(args) == 0 {
+		fmt.Println("[ERROR] Must set task")
+		return 0
+	}
+	title := strings.Join(args, " ")
+	fmt.Println("Added new task : ", title)
+
+	db, err := sql.Open("sqlite3", dbPath())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	now := strconv.FormatInt(time.Now().Unix(), 10)
+
+	_, err = db.Exec("INSERT INTO todos(title, is_done, created_at, updated_at) values('" + title + "', 0, " + now + ",  " + now + ")")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	return 0
 }
